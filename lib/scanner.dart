b111739adc1bcc1c835af1aa3531bc7f6cdd443a
Alok1721 <131133604+Alok1721@users.noreply.github.com>
2023-10-28 01:03:20 +0530

    Add files via upload

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:app3/event.dart';
+import 'package:app3/Dashboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,20 +14,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Barcode Scanner Example',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
+
       home: ScanScreen(),
     );
   }
 }
 
 class ScanScreen extends StatefulWidget {
+
+
+
+
   @override
   _ScanScreenState createState() => _ScanScreenState();
 }
 
 class _ScanScreenState extends State<ScanScreen> {
+
+
+
   String _scanBarcodeResult = "Scan a barcode"; // Initial result text
+
+
+  @override
+  void initState()
+  {
+    super.initState();
+    scanBarcodeNormal();
+  }
 
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
@@ -37,6 +55,8 @@ class _ScanScreenState extends State<ScanScreen> {
         ScanMode.BARCODE,
       );
       debugPrint(barcodeScanRes);
+      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>event(uid:barcodeScanRes)));
+
     } on PlatformException {
       barcodeScanRes = 'Failed to execute';
     }
@@ -51,6 +71,11 @@ class _ScanScreenState extends State<ScanScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('QR code scan'),
+        leading: IconButton(icon: Icon(Icons.arrow_back,color:Colors.black),
+          onPressed:()=>Navigator.of(context).pop(),
+
+
+          ), backgroundColor: Colors.orange.shade300,
       ),
       body: Center(
         child: Column(
@@ -58,6 +83,7 @@ class _ScanScreenState extends State<ScanScreen> {
           children: [
             ElevatedButton(
               onPressed: scanBarcodeNormal,
+              style: ElevatedButton.styleFrom(primary: Colors.orange.shade600 ),
               child: Text('Start QR code scan'),
             ),
             SizedBox(height: 20),
@@ -71,6 +97,13 @@ class _ScanScreenState extends State<ScanScreen> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(backgroundColor: Colors.orange.shade50,items:
+      [
+        BottomNavigationBarItem(icon: IconButton(icon: Icon(Icons.event), onPressed: () { Navigator.pushReplacement(context,MaterialPageRoute(builder: (_)=>event())); },),label:'events'),
+        BottomNavigationBarItem(icon: IconButton(icon: Icon(Icons.home), onPressed: () { Navigator.pushReplacement(context,MaterialPageRoute(builder: (_)=>Dashboard())); },),label:'Dashboard'),
+        BottomNavigationBarItem(icon: IconButton(icon: Icon(Icons.qr_code), onPressed: () { Navigator.push(context,MaterialPageRoute(builder: (_)=>ScanScreen())); },),label:'Scanner'),
+      ],
       ),
     );
   }
